b01473e82880bcb6dec1a402115e6bbe861b70d1
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-09-13 15:38:11 -0300

    Criando classe ContaBancaria

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,25 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private saldo = 20;
+    private numeroConta = 123;
+    private agencia = 89;
+    private extrato = 1
+
+
+    public depositar(valor:number){
+        if(valor > 0){
+            this.saldo += valor
+        }
+    }
+
+   
+
+    public sacar(valor: number){
+        if(valor >= this.saldo){
+            this.saldo -= valor
+        }
+    }
+
+    public consultarSaldo(){
+        return this.saldo
+    }
+}
