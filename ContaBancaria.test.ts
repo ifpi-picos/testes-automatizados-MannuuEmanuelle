9fa9adf18797dbed462629b298b5bb1c3c2dae2e
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-09-13 15:34:32 -0300

    Criando classe ContaBancaria

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,20 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
 
+describe('Testando classe ContaBancaria', () => {
+    let conta:ContaBancaria
+    beforeEach(() => {
+        conta = new ContaBancaria()
+    })
+
+    // test('Testando depositar valor positivo', () => {
+    //     conta.depositar(100)
+    //     expect(conta.consultarSaldo()).toBe(100)
+    // })
+
+    test('Testando sacar valor válido', () => {
+        conta.sacar(10)
+        expect(conta.consultarSaldo()).toBe(10)
+    })
+})
