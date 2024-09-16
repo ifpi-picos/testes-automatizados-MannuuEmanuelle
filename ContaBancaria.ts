6ab3dc43f5d97a69a7126434f519ed9d533ca10f
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-09-16 18:28:15 -0300

    Adicionando os métodos transferir e exibir extrato

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -2,24 +2,42 @@ export default class ContaBancaria {
     private saldo = 20;
     private numeroConta = 123;
     private agencia = 89;
-    private extrato = 1
+    private extrato: String[]
 
+    constructor(){
+        this.extrato = []
+    } 
 
-    public depositar(valor:number){
-        if(valor > 0){
+    public depositar(valor: number) {
+        if (valor > 0) {
             this.saldo += valor
+            this.extrato.push(`Depósito de R$${valor}, 00`)
         }
     }
 
-   
-
-    public sacar(valor: number){
-        if(valor >= this.saldo){
+    public sacar(valor: number) {
+        if (valor <= this.saldo) {
             this.saldo -= valor
+            this.extrato.push(`Saque de R$${valor}, 00`)
         }
     }
 
-    public consultarSaldo(){
+    public consultarSaldo() {
         return this.saldo
+    }
+
+    public transferir(valor: number, contaDestino: ContaBancaria) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor
+            contaDestino.saldo += valor
+            this.extrato.push(`Transferência de R$${valor}, 00`)
+        }
+    }
+
+    
+    public exibirExtrato() {
+        for(let extrato of this.extrato){
+            return extrato
+        }
     }
 }
