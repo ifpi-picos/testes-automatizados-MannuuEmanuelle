549d502cc5283cf06f34fa77c3bf2800164b90cf
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-09-16 18:29:22 -0300

    Testando o método transferir

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -4,17 +4,25 @@ import ContaBancaria from "./ContaBancaria.ts";
 
 describe('Testando classe ContaBancaria', () => {
     let conta:ContaBancaria
+    let contaDestino = new ContaBancaria()
     beforeEach(() => {
         conta = new ContaBancaria()
     })
 
     test('Testando depositar valor positivo', () => {
         conta.depositar(100)
-        expect(conta.consultarSaldo()).toBe(100)
+        expect(conta.consultarSaldo()).toBe(120)
     })
 
     test('Testando sacar valor válido', () => {
         conta.sacar(10)
         expect(conta.consultarSaldo()).toBe(10)
+    })
+
+    test('Testando transferir valor válido', () => {
+        conta.transferir(15, contaDestino)
+        expect(conta.consultarSaldo()).toBe(5)
+
+
     })
 })
